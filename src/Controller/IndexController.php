2f4158d991cdc2cf3449dd33d05fe2acd5b988c6
ireshniov <ireshniov@gmail.com>
2019-09-08 00:54:33 +0300

    keep in main routs configuration at least one route - homepage (not in annotations)

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,12 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class IndexController
+ * @package App\Controller
+ */
 class IndexController extends AbstractController
 {
     /**
-     * @Route(name="homepage", path="/")
      * @return Response
      */
     public function index()
