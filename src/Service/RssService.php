a72bbbd0e021cbacfe14adb524d4b1f2ae8b433b
ireshniov <ireshniov@gmail.com>
2019-09-09 21:37:06 +0300

    removed cache logic to simplify service; don`t know all dependencies;

--- src/Service/RssService.php
+++ src/Service/RssService.php
@@ -2,12 +2,10 @@
 
 namespace App\Service;
 
-use DateTime;
 use Exception;
 use FeedIo\Feed;
+use FeedIo\FeedInterface;
 use FeedIo\FeedIo;
-use Psr\Cache;
-use Psr\Log\LoggerInterface;
 
 /**
  * Class RssService
@@ -21,35 +19,17 @@ class RssService
     /** @var array */
     private $resourceMap;
 
-    /** @var Cache\CacheItemPoolInterface */
-    private $cache;
-
-    /** @var LoggerInterface */
-    private $logger;
-
-    /** @var string|null */
-    private $namespace;
-
     /**
      * RssService constructor.
      * @param FeedIo $feedIo
      * @param array $resourceMap
-     * @param Cache\CacheItemPoolInterface $cache
-     * @param LoggerInterface $logger
-     * @param string|null $namespace
      */
     public function __construct(
         FeedIo $feedIo,
-        array $resourceMap,
-        Cache\CacheItemPoolInterface $cache,
-        LoggerInterface $logger,
-        ?string $namespace = 'mintos_test'
+        array $resourceMap
     ) {
         $this->feedIo = $feedIo;
         $this->resourceMap = $resourceMap;
-        $this->cache = $cache;
-        $this->logger = $logger;
-        $this->namespace = $namespace;
     }
 
     /**
@@ -74,51 +54,21 @@ class RssService
      */
     public function getFeed(string $resourceName): array
     {
-        $cacheKey = $this->getCacheKey($resourceName);
+        //TODO add cache logic;
 
-        try {
-            $cacheItem = $this->cache->getItem($cacheKey);
-        } catch (Cache\InvalidArgumentException $exception) {
-            $this->logger->critical(sprintf("Key \"%\" string is not a legal value", $cacheKey), ['rss-service']);
-
-            return $this->fetchFeed($resourceName);
-        }
-
-        if ($cacheItem->isHit()) {
-            return $cacheItem->get();
-        }
-
-        $feed = $this->fetchFeed($resourceName);
-
-        $cacheItem
-            ->set($feed)
-            ->expiresAt(
-                (new DateTime())->modify('+1 day')
-            );
+        $feed = $this->fetchFeed($resourceName)->toArray();
 
         return $feed;
     }
 
     /**
      * @param string $resourceName
-     * @return array
+     * @return FeedInterface|Feed
      */
-    private function fetchFeed(string $resourceName): array
+    private function fetchFeed(string $resourceName): FeedInterface
     {
         $result = $this->feedIo->read($this->resourceMap[$resourceName]);
 
-        /** @var Feed $feed */
-        $feed = $result->getFeed();
-
-        return $feed->toArray();
-    }
-
-    /**
-     * @param string $resourceName
-     * @return string
-     */
-    private function getCacheKey(string $resourceName): string
-    {
-        return md5(serialize([$resourceName, $this->resourceMap[$resourceName]]) . $this->namespace);
+        return $result->getFeed();
     }
 }
